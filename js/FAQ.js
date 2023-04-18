606345894baf9d7fc87cc26e55b0657c57e4a5ba
Shruti Gupta <shgupta1811@gmail.com>
2023-04-18 19:17:50 +0530

    Add files via upload

---
+++ js/FAQ.js
@@ -0,0 +1,38 @@
+function myFunction(id) {
+    var x = id;
+    if(x=="que1"){
+        if (ans1.style.display == "none") {
+              ans1.style.display = "block";
+            } else {
+              ans1.style.display = "none";
+            }
+    }
+    if(x=="que2"){
+        if (ans2.style.display == "block") {
+              ans2.style.display = "none";
+            } else {
+              ans2.style.display = "block";
+            }
+    }
+    if(x=="que3"){ 
+        if (ans3.style.display == "block") {
+              ans3.style.display = "none";
+            } else {
+              ans3.style.display = "block";
+            }
+    }
+    if(x=="que4"){
+        if (ans4.style.display == "block") {
+              ans4.style.display = "none";
+            } else {
+              ans4.style.display = "block";
+            }
+    }
+    if(x=="que5"){
+        if (ans5.style.display == "block") {
+              ans5.style.display = "none";
+            } else {
+              ans5.style.display = "block";
+            }
+    }
+}
